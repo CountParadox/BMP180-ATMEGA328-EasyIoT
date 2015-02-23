fd413ab840214b6260a7d3ac90ce3305e9314b5b
Mark Greenaway <certifiedwaif@gmail.com>
2015-02-23 20:00:30 +1100

    Store all strings in flash memory rather than RAM using the F(). See http://playground.arduino.cc/Learning/Memory

--- ESP_Weather_Station/ESP_Weather_Station.ino
+++ ESP_Weather_Station/ESP_Weather_Station.ino
@@ -50,13 +50,13 @@ void setup()
   Serial.begin(115200); // debug
 
   if (bmp180.begin())
-    Serial.println("BMP180 init success");
+    Serial.println(F("BMP180 init success"));
    else
      {
     // Oops, something went wrong, this is usually a connection problem,
     // see the comments at the top of this sketch for the proper connections.
 
-    Serial.println("BMP180 init fail\n\n");
+    Serial.println(F("BMP180 init fail\n\n"));
     while(1); // Pause forever.
   }
    
@@ -113,11 +113,11 @@ void loop()
     if (status != 0)
     {
       // Print out the measurement:
-      Serial.print("temperature: ");
+      Serial.print(F("temperature: "));
       Serial.print(T,2);
-      Serial.print(" deg C, ");
+      Serial.print(F(" deg C, "));
       Serial.print((9.0/5.0)*T+32.0,2);
-      Serial.println(" deg F");
+      Serial.println(F(" deg F"));
       
            
       static int lastSendTempInt;
@@ -156,11 +156,11 @@ void loop()
           // Result: p0 = sea-level compensated pressure in mb
 
           p0 = bmp180.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
-          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(F("relative (sea-level) pressure: "));
           Serial.print(p0,2);
-          Serial.print(" mb, ");
+          Serial.print(F(" mb, "));
           Serial.print(p0*0.0295333727,2);
-          Serial.println(" inHg");
+          Serial.println(F(" inHg"));
           
           
           static int lastSendPresInt;
@@ -182,13 +182,13 @@ void loop()
             esp.send(msgForec.set(weather[forecast]));
           }
         }
-        else Serial.println("error retrieving pressure measurement\n");
+        else Serial.println(F("error retrieving pressure measurement\n"));
       }
-      else Serial.println("error starting pressure measurement\n");
+      else Serial.println(F("error starting pressure measurement\n"));
     }
-    else Serial.println("error retrieving temperature measurement\n");
+    else Serial.println(F("error retrieving temperature measurement\n"));
   }
-  else Serial.println("error starting temperature measurement\n");
+  else Serial.println(F("error starting temperature measurement\n"));
 
   startTime = millis();  
 }
